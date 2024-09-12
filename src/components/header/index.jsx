12b0651c8e004cdea9ad7a0869bdcd0db115ed97
jitindersinghtechy <jitindersingh371@gmail.com>
2024-09-12 16:56:28 +0530

    added search component and result search functionality

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from "react";
 import { IMAGES } from "../../constants/imagePath";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Dropdown } from "flowbite-react";
-import { IoIosLogOut } from "react-icons/io";
+import { IoIosLogOut, IoMdClose } from "react-icons/io";
 import { logout } from "../../redux/actions/authActions";
 import { deleteUserRequest } from "../../redux/actions/authActions";
 import { useDispatch } from "react-redux";
@@ -12,6 +12,7 @@ import { postRequest } from "../../axios/interceptor";
 import { debounce } from "lodash";
 import { contactTypeLabels } from "../../constants/constants";
 import { setSelectedContact } from "../../redux/actions/contactActions";
+import Search from "../search";
 
 const Header = ({ toggleDrawer, title }) => {
   const navigate = useNavigate();
@@ -40,11 +41,16 @@ const Header = ({ toggleDrawer, title }) => {
     debouncedFunction(value);
   };
 
+  const handleInputReset = () => {
+    setSearchValue('');
+    setSearchResults([]);
+  };
+
   const handleItemClick = (item) => {
     dispatch(setSelectedContact(item));
     navigate(ROUTES.CONTACT_PARTNER);
     setSearchResults([]);
-    setSearchValue(""); 
+    setSearchValue("");
   };
   const handleLogout = () => {
     dispatch(logout());
@@ -67,35 +73,18 @@ const Header = ({ toggleDrawer, title }) => {
           <input
             className="text-base text-secondary-700 font-normal leading-6 bg-bg-gray-100 py-2 px-6 rounded-[28px] w-[360px]"
             placeholder="Search case, contact or address"
-            value={searchValue} // Bind the value to the state
-            onChange={handleInputChange} // Update state on change
-            // onChange={(e) => {
-            //   // handleChange(e);
-            //   debouncedFunction(e.target.value);
-            // }}
+            value={searchValue}
+            onChange={handleInputChange}
           />
-          <img
-            src={IMAGES.searchIcon}
-            alt="icon"
-            className="absolute right-5 top-[10px]"
-          />
-          <ul className={'absolute left-0 right-0 mt-2 bg-white z-50 search-list-dropdown overflow-hidden rounded-2xl shadow-shadow-light-2'}>
-            {searchResults?.map((item) => (
-              <li
-                className={'px-4 py-2 hover:bg-input-surface'}
-                onClick={() => handleItemClick(item)} // Use the new handler
-                key={item.id} // Adding a key for each list item for better performance
-              >
-                <div className="flex items-center">
-                  <img src={IMAGES.contact_avtar} className="w-8 mr-3" />
-                  <div>
-                    <p className="text-base text-secondary-800">{item?.firstName} {item?.lastName}</p>
-                    <span className="text-text-gray-100 text-sm">{contactTypeLabels[item?.contactType] || ""}</span>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+          {searchResults?.length > 0 ?
+          <IoMdClose className="absolute right-5 top-[10px] cursor-pointer" onClick={handleInputReset} />
+            : <img
+              src={IMAGES.searchIcon}
+              alt="icon"
+              className="absolute right-5 top-[10px]"
+            />}
+          {searchResults.length > 0 ? <Search searchResults={searchResults} /> : ""}
+
         </div>
         <Dropdown
           arrowIcon={false}
