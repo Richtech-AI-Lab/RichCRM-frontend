0621bf6dd81cd8c58f7f1ae30087dcc151b194ce
jitindersinghtechy <jitindersingh371@gmail.com>
2024-09-17 12:51:37 +0530

    Implement dynamic add remove "toaddress" in compose email

--- src/components/composeEmail/index.js
+++ src/components/composeEmail/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import XButton from "../../components/button/XButton"
 import { IoIosClose } from 'react-icons/io';
 import logo from '../../assets/images/logo-dark.png'
@@ -6,34 +6,54 @@ import avatar from '../../assets/images/contact_avtar.png'
 import { useDispatch, useSelector } from 'react-redux';
 import { Formik } from 'formik';
 import { sendEmailRequest } from '../../redux/actions/utilsActions';
+import { Spinner } from 'flowbite-react';
 
 const ComposeEmail = ({ onClose }) => {
   const dispatch = useDispatch();
   const { client } = useSelector((state) => state.client);
+  // const { loading} = useSelector((state) => state.utils);
   const clientDetails = client?.data?.length > 0 ? client?.data : null;
+  const [inputValue, setInputValue] = useState('');
+  const [toEmail, setToEmail] = useState([]);
 
   // Initial values for the form fields
   const initialValues = {
-    toAddresses: [""], 
-    ccAddresses: [""], 
-    templateTitle: "", 
+    toAddresses: [""],
+    ccAddresses: [""],
+    templateTitle: "",
     templateContent: ""
   };
   const handleSubmit = (values) => {
-    let clientEmailArr=[]
-    let clientEmail=clientDetails && clientDetails[0]?.email
-    clientEmailArr.push(clientEmail)
-    const payload={
-      toAddresses:clientEmailArr, 
-      ccAddresses:clientEmailArr, 
-      templateTitle: values.templateTitle, 
+    const payload = {
+      toAddresses: toEmail,
+      ccAddresses: toEmail,
+      templateTitle: values.templateTitle,
       templateContent: values.templateContent
     }
     dispatch(sendEmailRequest(payload))
-    
+    onClose()
+
   };
+  // For To Email 
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  // For To Email 
+  const handleInputBlur = () => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailPattern.test(inputValue)) {
+      setToEmail((prevEmails) => [...prevEmails, inputValue]);
+      setInputValue('');
+    }
+  };
+  // For To Email 
+  const removeToEmail = (index) => {
+    const updatedEmails = toEmail.filter((_, i) => i !== index);
+    setToEmail(updatedEmails);
+  }
 
   return (
+    <>
     <div className="bg-white rounded-2xl shadow-card fixed bottom-3 right-3 w-[552px]">
       <div className="flex justify-between items-center p-4">
         <h3 className="text-base text-secondary-800 font-medium">Compose Message</h3>
@@ -59,21 +79,28 @@ const ComposeEmail = ({ onClose }) => {
               <div className="border-b border-b-border py-[6px] flex items-center">
                 <label className="inline text-sm font-medium text-text-gray-100 mr-2">To</label>
                 <ul>
-                  <li className="flex items-center p-2 bg-bg-gray-300 rounded-full">
-                    <img src={avatar} alt="" className="mr-2" />
-                    <span>{clientDetails && clientDetails[0]?.email}</span>
-                    <IoIosClose size={28} className="text-text-gray-100 cursor-pointer" />
-                  </li>
+                  {toEmail?.map((item, index) =>
+                    <li className="flex items-center p-2 bg-bg-gray-300 rounded-full">
+                      <img src={avatar} alt="" className="mr-2" />
+                      <span>{item}</span>
+                      <IoIosClose size={28} className="text-text-gray-100 cursor-pointer" onClick={() => removeToEmail(index)} />
+                    </li>
+                  )}
+
                 </ul>
                 <input
                   type="text"
                   className="inline border-0 focus:ring-transparent"
+                  value={inputValue}
+                  onChange={handleInputChange}
+                  onBlur={handleInputBlur} // or use onKeyDown to detect 'Enter' key
+                  placeholder="Enter email"
                 />
               </div>
               <div className="border-b border-b-border py-[6px]">
                 <label className="inline text-sm font-medium text-text-gray-100 mr-2">Subject</label>
                 <input
-                 name='templateTitle'
+                  name='templateTitle'
                   type="text"
                   // placeholder="Subject"
                   value={values.templateTitle}
@@ -103,6 +130,7 @@ const ComposeEmail = ({ onClose }) => {
                 <p className="text-sm text-text-gray-100 font-medium">Paralegal ; Office Manager </p>
               </div>
             </div>
+
             <div className="text-end px-4 py-3 shadow-full rounded-bl-2xl rounded-br-2xl">
               <XButton
                 text="Send"
@@ -114,6 +142,7 @@ const ComposeEmail = ({ onClose }) => {
         )}
       </Formik>
     </div>
+    </>
   );
 };
 
