58789946479ca3e1b12ef297bcc6da45cab8d93e
jitindersinghtechy <jitindersingh371@gmail.com>
2024-09-20 14:57:14 +0530

    update and integrate calender component

--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import { contactTab } from "../../constants/constants";
 import ContactsActionbar from "../../components/actionbar/contactsActionBar";
 import ContactListingV1 from "./contactListingV1";
+import { ROUTES } from "../../constants/api";
+import { useLocation } from "react-router-dom";
 
 const Contacts = () => {
   const [activeTab, setActiveTab] = useState(0);
-
+  const location = useLocation();
+  const shouldShow = (routePath) => {
+    const pathsToShow = [routePath];
+    return pathsToShow.includes(location.pathname);
+  };
   return (
-    <div className="mt-14">
+    <div className={shouldShow(ROUTES.DASHBOARD) ? "mt-2" : "mt-14" }>
       <ContactsActionbar active={activeTab} setActive={setActiveTab} />
       <ContactListingV1 active={activeTab} />
     </div>
