2d369a3b6ade103a6d814cdaf2b8f74050d02c37
jitindersinghtechy <jitindersingh371@gmail.com>
2024-10-14 16:39:50 +0530

    redirection for contact done.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -11,12 +11,17 @@ import { API_ENDPOINTS, ROUTES } from "../../constants/api";
 import { postRequest } from "../../axios/interceptor";
 import { debounce } from "lodash";
 import { contactTypeLabels } from "../../constants/constants";
-import { clearAttorney, setSelectedContact } from "../../redux/actions/contactActions";
+import {
+  clearAttorney,
+  setSelectedContact,
+} from "../../redux/actions/contactActions";
 import Search from "../search";
 import { persistor } from "../../redux/store";
 import { clearStageData } from "../../redux/actions/stagesActions";
 import { clearTaskData } from "../../redux/actions/taskActions";
 import { clearClientData } from "../../redux/actions/clientActions";
+import { setSelectedOrganization } from "../../redux/actions/organizationActions";
+import { fetchAllCasesRequest } from "../../redux/actions/caseAction";
 
 const Header = ({ toggleDrawer, title }) => {
   const navigate = useNavigate();
@@ -27,20 +32,35 @@ const Header = ({ toggleDrawer, title }) => {
   const debouncedFunction = useCallback(
     debounce(async (value, index) => {
       if (value != "" || value.length > 0) {
-        const contactResponse = await postRequest(API_ENDPOINTS.GET_CONTACT_BY_KEYWORD, {
-          keyword: value
-        })
-        const casesResponse = await postRequest(API_ENDPOINTS.GET_CASES_BY_KEYWORD, {
-          keyword: value
-        })
+        const contactResponse = await postRequest(
+          API_ENDPOINTS.GET_CONTACT_BY_KEYWORD,
+          {
+            keyword: value,
+          }
+        );
+        const casesResponse = await postRequest(
+          API_ENDPOINTS.GET_CASES_BY_KEYWORD,
+          {
+            keyword: value,
+          }
+        );
+        const organizationResponse = await postRequest(
+          API_ENDPOINTS.GET_ORGANIZATION_BY_KEYWORD,
+          {
+            keyword: value,
+          }
+        );
+        const orgabiRe = organizationResponse?.data?.data;
         const contactResults = contactResponse?.data?.data;
         const caseResults = casesResponse?.data?.data;
-        setSearchResults(prevResults => ({
-          ...prevResults,          
-          contact: contactResults,
-          case: caseResults
+        // Combine orgabiRe and contactResults
+        const combinedContacts = [...contactResults, ...orgabiRe];
+        setSearchResults((prevResults) => ({
+          ...prevResults,
+          contact: combinedContacts,
+          case: caseResults,
         }));
-        console.log(contactResults,caseResults)
+        console.log(contactResults, caseResults, orgabiRe);
       } else {
         setSearchResults([]);
       }
@@ -54,52 +74,88 @@ const Header = ({ toggleDrawer, title }) => {
   };
 
   const handleInputReset = () => {
-    setSearchValue('');
+    setSearchValue("");
     setSearchResults([]);
   };
 
   const handleClearAndLoader = async (casedetails) => {
     localStorage.setItem("c_id", casedetails?.caseId);
+    fetchAllCases();
     await Promise.all([
       dispatch(clearClientData()),
       dispatch(clearStageData()),
       dispatch(clearTaskData()),
-      dispatch(clearAttorney())
+      dispatch(clearAttorney()),
     ]);
-  
     console.log("State cleared and loader started");
   };
+
+  const fetchAllCases = async () => {
+    try {
+      const payload = {
+        creatorId: localStorage.getItem("authEmail"),
+        closed: false,
+      };
+      dispatch(fetchAllCasesRequest(payload));
+    } catch (error) {
+      console.error("Error fetching cases:", error);
+    }
+  };
+
   const debouncedNavigation = useCallback(
-    debounce(async (casedetails) => {
-      if (casedetails != "" || casedetails.length > 0) {
-        navigate(ROUTES.CASES_DATA, { state: { casedetails } });
-      
+    debounce(async (casedetails, route, state) => {
+      if (casedetails && Object.keys(casedetails).length > 0) {
+        navigate(route, { state });
+
         // Reset search state
         setSearchResults([]);
         setSearchValue("");
-      
       } else {
         setSearchResults([]);
       }
     }, 1000),
     []
   );
-  const handleItemClick = async (casedetails) => {  
-    await handleClearAndLoader(casedetails);
-    debouncedNavigation(casedetails)
-  };
-  
-  const handleLogout = () => {
-    persistor.purge().then(() => {
-      console.log('Logged out and persisted state cleared');}
-    )
-    dispatch(logout());
-    localStorage.removeItem('authEmail');
-    localStorage.setItem("headerTitle", 'Dashboard');
-    localStorage.clear()
-    navigate(ROUTES.LOGIN);
+
+  const handleItemClick = async (item) => {
+    console.log(item);
+
+    if (!item) return;
+    if (item?.caseId) {
+      await handleClearAndLoader(item);
+      debouncedNavigation(item, ROUTES.CASES_DATA, { casedetails: item });
+    } else if (item?.contactId) {
+      dispatch(setSelectedContact(null));
+      localStorage.removeItem("contact_id");
+      setTimeout(() => {
+        dispatch(setSelectedContact(item));
+        localStorage.setItem("contact_id", item.contactId);
+        debouncedNavigation(item, ROUTES.CONTACT_PARTNER, { active: 0 });
+      }, 0);
+    } else if (item?.organizationId) {
+      dispatch(setSelectedOrganization(null));
+      localStorage.removeItem("organization_id");
+
+      setTimeout(() => {
+        dispatch(setSelectedOrganization(item));
+        localStorage.setItem("organization_id", item.organizationId);
+        debouncedNavigation(item, ROUTES.CONTACT_PARTNER, { active: 1 });
+      }, 0);
+    }
+
+    return;
   };
 
+  const handleLogout = () => {
+    persistor.purge().then(() => {
+      console.log("Logged out and persisted state cleared");
+    });
+    dispatch(logout());
+    localStorage.removeItem("authEmail");
+    localStorage.setItem("headerTitle", "Dashboard");
+    localStorage.clear();
+    navigate(ROUTES.LOGIN);
+  };
 
   return (
     <div className="flex justify-between items-center">
@@ -117,15 +173,28 @@ const Header = ({ toggleDrawer, title }) => {
             value={searchValue}
             onChange={handleInputChange}
           />
-          {searchResults?.contact?.length > 0 || searchResults?.case?.length > 0 ?
-          <IoMdClose className="absolute right-5 top-[10px] cursor-pointer" onClick={handleInputReset} />
-            : <img
+          {searchResults?.contact?.length > 0 ||
+          searchResults?.case?.length > 0 ? (
+            <IoMdClose
+              className="absolute right-5 top-[10px] cursor-pointer"
+              onClick={handleInputReset}
+            />
+          ) : (
+            <img
               src={IMAGES.searchIcon}
               alt="icon"
               className="absolute right-5 top-[10px]"
-            />}
-          {searchResults?.contact?.length > 0 || searchResults?.case?.length > 0 ? <Search onNavigation={handleItemClick} searchResults={searchResults} /> : ""}
-
+            />
+          )}
+          {searchResults?.contact?.length > 0 ||
+          searchResults?.case?.length > 0 ? (
+            <Search
+              onNavigation={handleItemClick}
+              searchResults={searchResults}
+            />
+          ) : (
+            ""
+          )}
         </div>
         <Dropdown
           arrowIcon={false}
