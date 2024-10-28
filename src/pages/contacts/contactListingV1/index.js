9d119bf88abd8ed550c53a18ef4258040b900d53
jitindersinghtechy <jitindersingh371@gmail.com>
2024-10-28 19:02:03 +0530

    Worked on pagination functionality

--- src/pages/contacts/contactListingV1/index.js
+++ src/pages/contacts/contactListingV1/index.js
@@ -1,4 +1,4 @@
-import { Table } from "flowbite-react";
+import { Pagination, Table } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { ROUTES } from "../../../constants/api";
 import { useNavigate } from "react-router-dom";
@@ -13,26 +13,38 @@ import NewOrganizationContactModalV1 from "../../../components/contactModal/newO
 import { fetchOrganizationByTypeRequest, setSelectedOrganization } from "../../../redux/actions/organizationActions";
 
 const ContactListingV1 = ({ active, parent, activeFilter }) => {
-
   const dispatch = useDispatch();
   const navigate = useNavigate()
   const contact = useSelector((state) => state?.contact?.contact)
-  const { loading:loadingContact } = useSelector((state) => state?.contact)
+  // const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const { loading: loadingContact } = useSelector((state) => state?.contact)
 
   const organization = useSelector((state) => state?.organization?.organization)
-  const { loading:loadingOrg } = useSelector((state) => state?.organization)
+  const { loading: loadingOrg } = useSelector((state) => state?.organization)
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil((active === 0 ? contact?.length : organization?.length) / itemsPerPage);
+
+  // Update displayed data based on pagination
+  const paginatedData = (active === 0 ? contact : organization)?.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+  console.log(paginatedData, "paginatedData")
+
 
   const headers = {
     [addFromContactV1Tab.individuals]: ["Name", "Tag", "Organization", "Position", "Email", "Cell Phone"],
     [addFromContactV1Tab.organizations]: ["Name", "Tag", "Website", "Email", "Cell Phone"]
   };
-
+  const onPageChange = (page) => setCurrentPage(page);
   const handleNavigation = (item) => {
-    if(active == 0){
+    if (active == 0) {
       dispatch(setSelectedContact(item));
       localStorage.setItem("contact_id", item)
       navigate(ROUTES.CONTACT_PARTNER, { state: { active } });
-    }else{
+    } else {
       dispatch(setSelectedOrganization(item));
       localStorage.setItem("organization_id", item)
       navigate(ROUTES.CONTACT_PARTNER, { state: { active } });
@@ -67,6 +79,7 @@ const ContactListingV1 = ({ active, parent, activeFilter }) => {
     } else {
       fetchOrganizationByType();
     }
+    setCurrentPage(1)
 
   }, [active, activeFilter]);
 
@@ -117,91 +130,105 @@ const ContactListingV1 = ({ active, parent, activeFilter }) => {
   }
 
   return (
+    <>
+
     <div className={`overflow-x-auto h-[68vh] ${parent === 'dashboard' ? '' : 'contacts-table db-contacts'}`}>
-      <XSpinnerLoader loading={loadingContact || loadingOrg} size="lg" />
-      <Table>
-        {active === 0 && contact?.length > 0 ? (
-          <>
-            <Table.Head>
-              {header.map((key, index) => (
-                <Table.HeadCell key={index}>{key}</Table.HeadCell>
-              ))}
-            </Table.Head>
-            <Table.Body className="divide-y">
-              {contact?.map((user, index) => (
-                <Table.Row key={index} className="bg-white dark:border-gray-700 dark:bg-gray-800 cursor-pointer"
-                  onClick={() => handleNavigation(user)}
-                >
-                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    <div className="flex items-center">
-                      <img src={IMAGES.contact_avtar} alt="Profile" className="mr-3 rounded-full" />
-                      <span className="left-txt font-medium text-secondary-800">
-                        {user.firstName} {user.lastName}
-                      </span>
-                    </div>
-                  </Table.Cell>
-                  {header.includes("Tag") && (
-                    <Table.Cell>
-                      <span className={`bg-badge-${getTaskLabelAndColor(user.contactType, "color")} text-secondary-100 text-sm font-semibold py-1 px-3 rounded-full inline-block`}>
-                        {getTaskLabelAndColor(user.contactType, "label")}
-                      </span>
+        <XSpinnerLoader loading={loadingContact || loadingOrg} size="lg" />
+        <Table>
+          {active === 0 && contact?.length > 0 ? (
+            <>
+              <Table.Head>
+                {header.map((key, index) => (
+                  <Table.HeadCell key={index}>{key}</Table.HeadCell>
+                ))}
+              </Table.Head>
+              <Table.Body className="divide-y">
+                {paginatedData?.map((user, index) => (
+                  <Table.Row key={index} className="bg-white dark:border-gray-700 dark:bg-gray-800 cursor-pointer"
+                    onClick={() => handleNavigation(user)}
+                  >
+                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                      <div className="flex items-center">
+                        <img src={IMAGES.contact_avtar} alt="Profile" className="mr-3 rounded-full" />
+                        <span className="left-txt font-medium text-secondary-800">
+                          {user.firstName} {user.lastName}
+                        </span>
+                      </div>
                     </Table.Cell>
-                  )}
-                  {header.includes("Organization") && <Table.Cell>{user.company}</Table.Cell>}
-                  {header.includes("Position") && <Table.Cell>{user.position}</Table.Cell>}
-                  {header.includes("Email") && <Table.Cell>{user.email}</Table.Cell>}
-                  {header.includes("Cell Phone") && <Table.Cell>{user.cellNumber}</Table.Cell>}
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </>
-        ) : active === 1 && organization?.length > 0 ? (
-          <>
-            <Table.Head>
-              {header.map((key, index) => (
-                <Table.HeadCell key={index}>{key}</Table.HeadCell>
-              ))}
-            </Table.Head>
-            <Table.Body className="divide-y">
-              {organization?.map((org, index) => (
-                <Table.Row key={index} className="bg-white dark:border-gray-700 dark:bg-gray-800 cursor-pointer"
-                  onClick={() => handleNavigation(org)}
-                >
-                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    <div className="flex items-center">
-                      <img src={IMAGES.contact_avtar} alt="Profile" className="mr-3 rounded-full" />
-                      <span className="left-txt font-medium text-secondary-800">
-                        {org?.organizationName}
-                      </span>
-                    </div>
-                  </Table.Cell>
-                  {header.includes("Tag") && (
-                    <Table.Cell>
-                      <span className="bg-badge-yellow text-secondary-100 text-sm font-semibold py-1 px-3 rounded-full inline-block">
-                        {ORGANIZATION_TYPE[org?.organizationType]}
-                      </span>
+                    {header.includes("Tag") && (
+                      <Table.Cell>
+                        <span className={`bg-badge-${getTaskLabelAndColor(user.contactType, "color")} text-secondary-100 text-sm font-semibold py-1 px-3 rounded-full inline-block`}>
+                          {getTaskLabelAndColor(user.contactType, "label")}
+                        </span>
+                      </Table.Cell>
+                    )}
+                    {header.includes("Organization") && <Table.Cell>{user.company}</Table.Cell>}
+                    {header.includes("Position") && <Table.Cell>{user.position}</Table.Cell>}
+                    {header.includes("Email") && <Table.Cell>{user.email}</Table.Cell>}
+                    {header.includes("Cell Phone") && <Table.Cell>{user.cellNumber}</Table.Cell>}
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </>
+          ) : active === 1 && organization?.length > 0 ? (
+            <>
+              <Table.Head>
+                {header.map((key, index) => (
+                  <Table.HeadCell key={index}>{key}</Table.HeadCell>
+                ))}
+              </Table.Head>
+              <Table.Body className="divide-y">
+                {organization?.map((org, index) => (
+                  <Table.Row key={index} className="bg-white dark:border-gray-700 dark:bg-gray-800 cursor-pointer"
+                    onClick={() => handleNavigation(org)}
+                  >
+                    <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                      <div className="flex items-center">
+                        <img src={IMAGES.contact_avtar} alt="Profile" className="mr-3 rounded-full" />
+                        <span className="left-txt font-medium text-secondary-800">
+                          {org?.organizationName}
+                        </span>
+                      </div>
                     </Table.Cell>
-                  )}
-                  {header.includes("Website") && <Table.Cell>{org?.website}</Table.Cell>}
-                  {header.includes("Email") && <Table.Cell>{org?.email}</Table.Cell>}
-                  {header.includes("Cell Phone") && <Table.Cell>{org?.cellNumber}</Table.Cell>}
-                </Table.Row>
-              ))}
-            </Table.Body>
-          </>
-        ) : (
-          <div className="flex flex-col items-center justify-center h-[60vh] w-full">
-            <p className="text-center text-gray-500">
-              No {active === 0 ? CONTACT_TYPE[activeFilter] : ORGANIZATION_TYPE[activeFilter]} Contact Available
-            </p>
-            <ContactButtonWithModal
-              buttonClass="bg-active-blue shadow-shadow-light text-sm text-active-blue-text py-[10px] px-6 rounded-[100px] font-medium mt-4"
-              modalContent={active === 0 ? <NewIndividualContactModalV1 /> : <NewOrganizationContactModalV1 />}
-            />
-          </div>
-        )}
-      </Table>
+                    {header.includes("Tag") && (
+                      <Table.Cell>
+                        <span className="bg-badge-yellow text-secondary-100 text-sm font-semibold py-1 px-3 rounded-full inline-block">
+                          {ORGANIZATION_TYPE[org?.organizationType]}
+                        </span>
+                      </Table.Cell>
+                    )}
+                    {header.includes("Website") && <Table.Cell>{org?.website}</Table.Cell>}
+                    {header.includes("Email") && <Table.Cell>{org?.email}</Table.Cell>}
+                    {header.includes("Cell Phone") && <Table.Cell>{org?.cellNumber}</Table.Cell>}
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </>
+          ) : (
+            <div className="flex flex-col items-center justify-center h-[60vh] w-full">
+              <p className="text-center text-gray-500">
+                No {active === 0 ? CONTACT_TYPE[activeFilter] : ORGANIZATION_TYPE[activeFilter]} Contact Available
+              </p>
+              <ContactButtonWithModal
+                buttonClass="bg-active-blue shadow-shadow-light text-sm text-active-blue-text py-[10px] px-6 rounded-[100px] font-medium mt-4"
+                modalContent={active === 0 ? <NewIndividualContactModalV1 /> : <NewOrganizationContactModalV1 />}
+              />
+            </div>
+          )}
+        </Table>
     </div>
+          {paginatedData?.length > 0 && <div className="flex overflow-x-auto sm:justify-center">
+            <Pagination
+              layout="pagination"
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={onPageChange}
+              previousLabel="Go back"
+              nextLabel="Go forward"
+              showIcons
+            />
+          </div>}
+          </>
   );
 };
 
