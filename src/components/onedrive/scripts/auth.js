18dca6328077de155b400148e09e18264fdbd483
EdenWuyifan <yfw215@nyu.edu>
2024-12-04 09:31:47 -0500

    bugfix: fix redirect uri

--- src/components/onedrive/scripts/auth.js
+++ src/components/onedrive/scripts/auth.js
@@ -3,8 +3,8 @@ export const msalParams = {
         // authority: `https://login.microsoftonline.com/${process.env.ONEDRIVE_TENANT_ID}`,
         authority: "https://login.microsoftonline.com/common",
         clientId: process.env.REACT_APP_ONEDRIVE_CLIENT_ID,
-        redirectUri: process.env.REACT_APP_NODE_ENV == "development" ? "http://localhost:3000" : process.env.REACT_APP_ONEDRIVE_REDIRECT_URL,
-        postLogoutRedirectUri: process.env.REACT_APP_NODE_ENV == "development" ? "http://localhost:3000" : process.env.REACT_APP_ONEDRIVE_REDIRECT_URL,
+        redirectUri: process.env.NODE_ENV === "development" ? "http://localhost:3000/rich-crm/documents" : process.env.REACT_APP_ONEDRIVE_REDIRECT_URL,
+        postLogoutRedirectUri: process.env.NODE_ENV === "development" ? "http://localhost:3000/rich-crm/documents" : process.env.REACT_APP_ONEDRIVE_REDIRECT_URL,
     },
 }
 
