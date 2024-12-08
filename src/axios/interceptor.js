8514b3d95e2911018d593d975167ea62b581e033
ProgRiseK <hnxhu2065@gmail.com>
2024-12-07 22:54:40 -0500

    feat: token handling in both req and res, check token expiration

--- src/axios/interceptor.js
+++ src/axios/interceptor.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 const axiosInstance = axios.create({
   baseURL:process.env.REACT_APP_BASE_URL,
@@ -7,11 +8,36 @@ const axiosInstance = axios.create({
   },
 });
 
+const isTokenExpired = (token) => { try { return jwtDecode(token).exp * 1000 < Date.now(); } catch { return true; } };
+
 axiosInstance.interceptors.request.use(
-  (request) => {
+  async (request) => {
     const authToken = localStorage.getItem("authToken");
-    if (authToken) {
+
+    if (!authToken) return request;
+    
+    if (!isTokenExpired(authToken)) {
       request.headers["Authorization"] = `Bearer ${authToken}`;
+    }
+    else {
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const refreshResponse = await axios.post("/auth/refresh", { refreshToken }, { withCredentials: true });
+        const { access: newAccessToken} = refreshResponse.data[0].token.access;
+
+        if (newAccessToken) {
+          localStorage.setItem("authToken", newAccessToken);
+          request.headers["Authorization"] = `Bearer ${newAccessToken}`;
+        }
+        else {
+          localStorage.clear();
+          window.location.href = "/";
+        }
+      } catch (error) {
+        localStorage.clear();
+        window.location.href = "/";
+        return Promise.reject(error);
+      }
     }
 
     return request;
@@ -23,8 +49,15 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
   (response) => {
+    const { access: accessToken, refresh: refreshToken } = response?.data?.data?.[0]?.token ?? {};
+    if (accessToken) {
+      localStorage.setItem("authToken", accessToken);
+    }
+    if (refreshToken) {
+      localStorage.setItem("refreshToken", refreshToken);
+    }
     if (response.status === 208) {
-      localStorage.removeItem("authToken");
+      localStorage.clear();
       window.location.href = "/";
     }
     return response;
@@ -32,6 +65,8 @@ axiosInstance.interceptors.response.use(
   (error) => {
     if (error.response && error.response.status === 404) {
       console.error("Resource not found");
+    } else if (error.response && error.response.status === 401) {
+      console.error("Unauthorized - token might be expired")
     } else if (error.response && error.response.status === 500) {
       console.error("Server error - please try again later");
     }
