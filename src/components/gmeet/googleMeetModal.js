5600ea4dcc719afcee07137069c8a59ff84711ad
jitindersinghtechy <jitindersingh371@gmail.com>
2024-12-19 17:01:34 +0530

    recreated design of create event modal

--- src/components/gmeet/googleMeetModal.js
+++ src/components/gmeet/googleMeetModal.js
@@ -14,8 +14,11 @@ import TextInput from "../TextInput";
 import DateTimeInput from "../dateTimePicker";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
-import { IoIosClose } from "react-icons/io";
+import { IoIosClose, IoIosLogOut } from "react-icons/io";
 import avatar from '../../assets/images/contact_avtar.png'
+import { IMAGES } from "../../constants/imagePath";
+import NewCaseDropdown from "../newcasedropdown";
+import language from "../../constants/language.json";
 
 const GoogleMeetModal = ({ onClose, title }) => {
   const { casesData } = useSelector((state) => state.case);
@@ -29,6 +32,7 @@ const GoogleMeetModal = ({ onClose, title }) => {
   const [toEmail, setToEmail] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [showParticipant, setShowParticipant] = useState(false);
 
   // Initialize GAPI client on component mount
   useEffect(() => {
@@ -131,28 +135,6 @@ const GoogleMeetModal = ({ onClose, title }) => {
   };
 
 
-  // Handle sign-in
-  // const handleSignIn = async () => {
-  //   try {
-  //     await signInToGoogle();
-  //     setAuth(true)
-  //     toast.success("Sign In!");
-  //   } catch (error) {
-  //     console.error("Sign-in failed:", error);
-  //     setAuth(false)
-  //   }
-  // };
-
-  // // Handle sign-out
-  // const handleSignOut = () => {
-  //   signOutFromGoogle();
-  //   // alert("Signed out successfully!");
-  //   setAuth(false)
-  //   toast.success("Sign Out!");
-  // };
-
-
-
   const handleFetchEvents = async () => {
     try {
       const fetchedEvents = await fetchUpcomingEvents(); // Ensure this returns an array of events
@@ -165,7 +147,7 @@ const GoogleMeetModal = ({ onClose, title }) => {
 
   // Create a new event
   const handleCreateEvent = async (values, { resetForm }) => {
-    if (values.title && values.description && values.endTime && values.startTime) {
+    if (values.title && values.endTime && values.startTime) {
       const newEvent = {
         summary: values.title,
         description: values.description,
@@ -236,140 +218,161 @@ const GoogleMeetModal = ({ onClose, title }) => {
   return (
     <Modal show={true} size="md" onClose={onClose} className="new-case-modal">
       <Modal.Header className="border-b-0">
-        <h2 className="text-4 font-medium text-secondary-800">Add Meeting</h2>
+        {title && <div className="flex flex-col justify-center items-start gap-2 self-stretch">
+          <span className="font-sans text-[28px] font-medium leading-[36px]">Create an Event</span>
+          <p className="font-sans text-sm font-normal leading-[20px] tracking-[0.25px]">Create a event by filling the basic information about the event.</p>
+        </div>}
       </Modal.Header>
-      <Modal.Body className="pt-3">
-         { isLoading ?
-         <div className="flex justify-center">
-          <Spinner
-                  size="xl"
-                  animation="border"
-                  role="status"
-                  variant="primary"
-                // className={`spinner-${size}`}
-                >
-                  <span className="visually-hidden">Loading...</span>
-                </Spinner> 
-          </div>:<Formik
-            initialValues={initialValues}
-            // validationSchema={validationSchema}
-            onSubmit={handleCreateEvent}
-            enableReinitialize
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-              setFieldValue
-            }) => (
+      <Modal.Body className="flex items-center self-stretch p-3 px-4">
+        <div className="flex flex-col gap-8 w-full">
 
-              <form onSubmit={handleSubmit} className="">
-                <div className="block">
+          {!auth && <div className="flex flex-col justify-center items-center"><XButton
+            text="Sign In"
+            icon={<FiPlus className="text-base mr-2 inline-block" />}
+            className="bg-active-blue shadow-shadow-light text-sm text-active-blue-text py-[10px] px-6 rounded-[100px] font-medium"
+            onClick={handleSignIn}
+          />
+          </div>
+          }
+          {isLoading ?
+            <div className="flex justify-center">
+              <Spinner
+                size="xl"
+                animation="border"
+                role="status"
+                variant="primary"
+              // className={`spinner-${size}`}
+              >
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </div> : <Formik
+              initialValues={initialValues}
+              // validationSchema={validationSchema}
+              onSubmit={handleCreateEvent}
+              enableReinitialize
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                setFieldValue
+              }) => (
 
-                  <div className="grid grid-cols-1 mt-3">
-                    {!auth ? <XButton
-                      text="Sign In"
-                      icon={<FiPlus className="text-base mr-2 inline-block" />}
-                      className="bg-active-blue shadow-shadow-light text-sm text-active-blue-text py-[10px] px-6 rounded-[100px] font-medium ml-4"
-                      onClick={handleSignIn}
-                    /> :
-                      <XButton
-                        text="Sign Out"
-                        icon={<FiPlus className="text-base mr-2 inline-block" />}
-                        className="bg-active-blue shadow-shadow-light text-sm text-active-blue-text py-[10px] px-6 rounded-[100px] font-medium ml-4"
-                        onClick={handleSignOut}
-                      />}
-                    {/* <XButton
-                      text="Fetch Events"
-                      icon={<FiPlus className="text-base mr-2 inline-block" />}
-                      className="bg-active-blue shadow-shadow-light text-sm text-active-blue-text py-[10px] px-6 rounded-[100px] font-medium ml-4"
-                      onClick={handleFetchEvents}
-                    />
-                    <XButton
-                      text="Create Event"
-                      icon={<FiPlus className="text-base mr-2 inline-block" />}
-                      className="bg-active-blue shadow-shadow-light text-sm text-active-blue-text py-[10px] px-6 rounded-[100px] font-medium ml-4"
-                      onClick={handleCreateEvent}
-                    /> */}
-                  </div>
-
-                </div>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-8 w-full">
 
 
-                {auth && <>
-                  <div className="grid grid-cols-1">
-                  <TextInput
-                      name="title"
-                      type="text"
-                      placeholder="Title"
-                      value={values.title}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      field={{ name: "title" }}
-                      form={{ errors, touched }}
-                      data-lpignore="true"
-                    />
-                    <TextInput
-                      name="description"
-                      type="text"
-                      placeholder="Description"
-                      value={values.description}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      field={{ name: "description" }}
-                      form={{ errors, touched }}
-                      data-lpignore="true"
-                    />
-                  </div>
+                  {auth && <>
+                    <div className="flex flex-col items-start gap-4 self-stretch">
+                      <label className="text-base font-medium leading-6 tracking-wide">Participants</label>
+                      <div className="flex items-start justify-between gap-4 self-stretch bg-bg-gray-200 rounded-full px-1 py-1">
+                        <ul className="flex items-center">
+                          {toEmail?.map((item, index) =>
+                            <li className="flex items-center justify-between p-2 bg-white rounded-full">
+                              <div className='flex items-center'>
+                                <img src={avatar} alt="" className="mr-2" />
+                                <span className='overflow-hidden'>{item}</span>
+                              </div>
+                              <IoIosClose size={28} className="text-text-gray-100 cursor-pointer"
+                                onClick={() => removeToEmail(index)}
+                              />
+                            </li>
+                          )}
 
-                  <div className="grid grid-cols-2 mt-3">
-                    
-                    <DateTimeInput
-                      name="startTime"
-                      value={values.startTime}
-                      placeHolder="Select Start Time"
-                      onSelectedDateChanged={(date) => setFieldValue("startTime", date)}
+                        </ul>
+                        <input
+                          type="text"
+                          className="inline border-0 focus:ring-transparent bg-bg-gray-200"
+                          value={inputValue}
+                          onChange={handleInputChange}
+                          onBlur={handleInputBlur} // or use onKeyDown to detect 'Enter' key
+                          placeholder="Enter email"
+                        />
+                        {!showParticipant ?
+                          <span className="flex w-10 h-10 flex-col justify-center items-center gap-2.5 cursor-pointer" onClick={() => setShowParticipant(true)}>
+                            <img src={IMAGES.addIcon} alt="icon" />
+                          </span> :
+                          <span className="flex w-10 h-10 flex-col justify-center items-center gap-2.5 cursor-pointer" onClick={() => setShowParticipant(false)}>
+                            <img src={IMAGES.removeIcon} alt="icon" />
+                          </span>}
+                      </div>
 
-                    />
-                    <DateTimeInput
-                      name="endTime"
-                      value={values.endTime}
-                      placeHolder="Select End Time"
-                      onSelectedDateChanged={(date) => setFieldValue("endTime", date)}
+                      {/* {showParticipant && <ParticipantListEmail setToEmail={setToEmail} toEmail={toEmail} onClose={() => setShowParticipant(prevState => !prevState)} />}
+                    {searchResults?.length > 0 && <SearchListEmail setInputValue={setInputValue} searchResults={searchResults} setSearchResults={setSearchResults} setToEmail={setToEmail} onClose={() => setShowParticipant(prevState => !prevState)} />} */}
 
-                    />
-
-                  </div>
-                    <div className="border-b border-b-border py-[6px] items-center">
-                      <label className="inline text-sm font-medium text-text-gray-100 mr-2">Participants</label>
-                      <ul>
-                        {toEmail?.map((item, index) =>
-                          <li className="flex items-center justify-between p-2 bg-bg-gray-300 rounded-full mb-2">
-                            <div className='flex items-center'>
-                              <img src={avatar} alt="" className="mr-2" />
-                              <span className='overflow-hidden'>{item}</span>
-                            </div>
-                            <IoIosClose size={28} className="text-text-gray-100 cursor-pointer"
-                              onClick={() => removeToEmail(index)}
-                            />
-                          </li>
-                        )}
-
-                      </ul>
-                      <input
+                    </div>
+                    <div className='flex flex-col gap-4 self-stretch'>
+                      <div className={`text-base font-medium leading-6 tracking-wide`}>Event Type</div>
+                      <div className={`items-dropdown single-select gray-btn `}  >
+                        <NewCaseDropdown
+                          defaultLabel="Online Meeting (Google Meet)"
+                          name="state"
+                          // value={values.state}
+                          // onChange={handleChange}
+                          // onBlur={handleBlur}
+                          options={[]}
+                        // field={{ name: "state" }}
+                        // form={{ errors, touched }}
+                        />
+                      </div>
+                    </div>
+                    <div className="hidden">
+                      <TextInput
+                        name="title"
                         type="text"
-                        className="inline border-0 focus:ring-transparent w-full"
-                        value={inputValue}
-                        onChange={handleInputChange}
-                        onBlur={handleInputBlur} // or use onKeyDown to detect 'Enter' key
-                        placeholder="Enter email"
+                        placeholder="Title"
+                        value={values.title}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        field={{ name: "title" }}
+                        form={{ errors, touched }}
+                        data-lpignore="true"
+                      />
+                      <TextInput
+                        name="description"
+                        type="text"
+                        placeholder="Description"
+                        value={values.description}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        field={{ name: "description" }}
+                        form={{ errors, touched }}
+                        data-lpignore="true"
                       />
                     </div>
-                    <div className="text-end mt-8">
+
+                    <div className="flex flex-col gap-4 self-stretch">
+                      <div className={`text-base font-medium leading-6 tracking-wide`}>Time</div>
+                      <div className="flex flex-row justify-around">
+                        <DateTimeInput
+                          name="startTime"
+                          value={values.startTime}
+                          placeHolder="Select Start Time"
+                          onSelectedDateChanged={(date) => setFieldValue("startTime", date)}
+
+                        />
+                        <DateTimeInput
+                          name="endTime"
+                          value={values.endTime}
+                          placeHolder="Select End Time"
+                          onSelectedDateChanged={(date) => setFieldValue("endTime", date)}
+
+                        />
+
+                      </div>
+                    </div>
+
+
+                    <div className="flex gap-4 justify-end">
+                      <XButton
+                        text="Sign Out"
+                        icon={<IoIosLogOut className="text-base mr-2 inline-block" />}
+                        className="bg-badge-red shadow-shadow-light text-sm text-primary2 py-[10px] px-6 rounded-[100px] font-medium ml-4"
+                        onClick={handleSignOut}
+                      />
                       <XButton
                         text={"Cancel"}
                         onClick={onClose}
@@ -378,16 +381,18 @@ const GoogleMeetModal = ({ onClose, title }) => {
                       />
                       <XButton
                         type="submit"
-                        text={"Create"}
+                        text={"Start Creating"}
                         disabled={isSubmitting}
-                        className="bg-primary text-sm text-white py-[10px] px-6 rounded-[100px] ml-4"
+                        className="bg-primary text-sm text-white py-[10px] px-6 rounded-[100px]"
                       />
                     </div>
+
                   </>
-                }
-              </form>
-            )}
-          </Formik>}
+                  }
+                </form>
+              )}
+            </Formik>}
+        </div>
       </Modal.Body>
     </Modal>
   );
