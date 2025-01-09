0f25ae63d8c1ddba3b9267b5c9b5ac6d5a91aa1e
jitindersinghtechy <jitindersingh371@gmail.com>
2025-01-09 22:05:10 +0530

    Fixed - new added task not showing untill refresh

--- src/redux/sagas/stagesSaga.js
+++ src/redux/sagas/stagesSaga.js
@@ -1,7 +1,7 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
 import { CREATE_STAGE_REQUEST, GET_STAGE_REQUEST, LINK_TASK_INTO_STAGE_REQUEST, UPDATE_STATUS_STAGE_REQUEST } from '../type';
 import { API_ENDPOINTS } from '../../constants/api';
-import { createStageFailure, createStageSuccess, startStageLoading } from '../actions/stagesActions';
+import { createStageFailure, createStageSuccess, getStageRequest, LinkTaskStageSuccess, startStageLoading } from '../actions/stagesActions';
 import { getRequest, postRequest } from '../../axios/interceptor';
 import { handleError } from '../../utils/eventHandler';
 
@@ -46,7 +46,12 @@ function* linkTaskStageSaga(action) {
   // console.log(payload,"______")
   try {
     const response = yield call(() => postRequest(API_ENDPOINTS.UPDATE_STAGE, payload));
-    // yield put(updateTaskStatusSuccess(data));
+    let stagePayload = {
+      stageType: response?.data?.data[0]?.stageType || 0,
+      caseId: localStorage.getItem('c_id'), // we can use response case id 
+    }
+    yield put(getStageRequest(stagePayload));
+    // yield put(LinkTaskStageSuccess(response));
   } catch (error) {
     handleError(error)
     // yield put(updateTaskStatusFailure(error.response?.data || error));
@@ -57,5 +62,5 @@ export function* stagesSaga() {
   yield takeLatest(CREATE_STAGE_REQUEST, createStageSaga);
   yield takeLatest(GET_STAGE_REQUEST, getStageSaga);
   yield takeLatest(UPDATE_STATUS_STAGE_REQUEST, updateStatusStageSaga);
-  yield takeLatest(LINK_TASK_INTO_STAGE_REQUEST, linkTaskStageSaga );
+  yield takeLatest(LINK_TASK_INTO_STAGE_REQUEST, linkTaskStageSaga);
 }
